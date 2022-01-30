3775d38e09b5e4756af1f51dc8419894046f4094
Mateus D'Almeida <mateusdalmeida0@gmail.com>
2022-01-30 17:35:50 -0300

    layout of product screen

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,4 +1,5 @@
 import 'package:agence/models/product_model.dart';
+import 'package:agence/pages/home/home.dart';
 import 'package:agence/widgets/custom_drawer.dart';
 import 'package:flutter/material.dart';
 
@@ -14,11 +15,108 @@ class Product extends StatelessWidget {
         centerTitle: true,
         title: Text(product.title),
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [Text(product.title), Text(product.description)],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.3,
+              child: const Center(
+                child: Text("MAPA"),
+              ),
+            ),
+            Row(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.network(product.imageUrl)),
+                ),
+                Flexible(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          product.title,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        Text(
+                          product.description,
+                          textAlign: TextAlign.justify,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: kToolbarHeight,
+        child: TextButton.icon(
+          style: TextButton.styleFrom(
+            primary: Colors.white,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.zero),
+            ),
+          ),
+          icon: const Icon(Icons.add_shopping_cart),
+          label: const Text("Comprar"),
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return SizedBox(
+                  height: 200,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      const Text(
+                        "Deseja prosseguir com a compra?",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            child: const Text("Não"),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                          ElevatedButton(
+                            child: const Text("Sim"),
+                            onPressed: () {
+                              const snackBar = SnackBar(
+                                content: Text('Compra realizada com sucesso'),
+                                behavior: SnackBarBehavior.floating,
+                              );
+
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+
+                              Navigator.of(context).pushAndRemoveUntil(
+                                  MaterialPageRoute(
+                                      builder: (context) => const Home()),
+                                  (Route<dynamic> route) => false);
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
